993d28b959362ff2d11b06b89f0f764d5625ee5c
priyadharshini-debug <priyadharshini@ustudyglobal.in>
2026-03-06 17:13:47 +0530

    Add files via upload
    Added To-Do app project files (HTML, CSS, JS)

--- script.js
+++ script.js
@@ -1,30 +1,143 @@
-function addTask() {
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    let taskInput = document.getElementById("taskInput");
-    let taskText = taskInput.value;
+function save(){
 
-    if(taskText === ""){
-        alert("Please enter a task");
-        return;
-    }
+localStorage.setItem("tasks",JSON.stringify(tasks));
 
-    let li = document.createElement("li");
-    li.textContent = taskText;
-
-    li.onclick = function(){
-        li.style.textDecoration = "line-through";
-    };
-
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.onclick = function(){
-        li.remove();
-    };
-
-    li.appendChild(deleteBtn);
-
-    document.getElementById("taskList").appendChild(li);
-
-    taskInput.value = "";
 }
+
+
+function render(){
+
+let list = document.getElementById("taskList");
+
+list.innerHTML="";
+
+let completed = 0;
+
+tasks.forEach((task,index)=>{
+
+let li = document.createElement("li");
+
+li.classList.add("priority"+task.priority);
+
+if(task.completed){
+
+li.classList.add("completed");
+
+completed++;
+
+}
+
+li.innerHTML=`
+
+<div onclick="toggle(${index})">
+
+<strong>${task.text}</strong>
+
+<br>
+
+<small>Priority: ${task.priority} | Due: ${task.date}</small>
+
+</div>
+
+<button class="deleteBtn" onclick="removeTask(${index})">Delete</button>
+
+`;
+
+list.appendChild(li);
+
+});
+
+
+let percent = tasks.length ? (completed/tasks.length)*100 : 0;
+
+document.getElementById("progressBar").style.width = percent+"%";
+
+document.getElementById("taskStats").innerText =
+`Completed ${completed} of ${tasks.length} tasks`;
+
+save();
+
+}
+
+
+function addTask(){
+
+let text = document.getElementById("taskInput").value;
+
+let priority = document.getElementById("priority").value;
+
+let date = document.getElementById("dueDate").value;
+
+if(text===""){
+
+alert("Enter a task");
+
+return;
+
+}
+
+tasks.push({
+
+text:text,
+priority:priority,
+date:date,
+completed:false
+
+});
+
+document.getElementById("taskInput").value="";
+
+render();
+
+}
+
+
+function toggle(index){
+
+tasks[index].completed = !tasks[index].completed;
+
+render();
+
+}
+
+
+function removeTask(index){
+
+tasks.splice(index,1);
+
+render();
+
+}
+
+
+function searchTask(){
+
+let input = document.getElementById("searchInput").value.toLowerCase();
+
+let items = document.querySelectorAll("li");
+
+items.forEach(task=>{
+
+let text = task.innerText.toLowerCase();
+
+task.style.display = text.includes(input) ? "flex" : "none";
+
+});
+
+}
+
+
+/* DARK MODE */
+
+let toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.onclick = function(){
+
+document.body.classList.toggle("dark");
+
+};
+
+
+render();
